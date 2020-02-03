1cf00b5733f87f313bf2c7ee4c986fed1259601b
Roman <romansklepovyy1@gmail.com>
2020-02-03 22:26:11 +0200

    Exp, Inc objects

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 var budgetController = (function () {
 
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+
+        allItems: {
+            inc: [],
+            exp: []
+        },
+
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    };
 
 })();
 

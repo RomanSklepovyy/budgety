e0cf3ecba232485962a21b720f5d55248dc43797
Roman <romansklepovyy1@gmail.com>
2020-02-08 16:32:49 +0200

    clearFields

--- app.js
+++ app.js
@@ -25,6 +25,37 @@ var budgetController = (function () {
         }
     };
 
+    return {
+
+        addItem: function (type, des, val) {
+            var newItem, id;
+
+            // Create new id
+            if (data.allItems[type].length > 0) {
+                id = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                id = 0;
+            }
+
+            // New item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(id, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(id, des, val);
+            }
+
+            // Push into data structure
+            data.allItems[type].push(newItem);
+
+            // Return new element
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
+        }
+    }
+
 })();
 
 
@@ -35,16 +66,71 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
         getInput: function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // will be inc or exp
-                description: document.querySelectorAll(DOMstrings.inputDescription).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
                 inputValue: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            if(type === 'inc') {
+
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="income-%id%">\n' +
+                    '                            <div class="item__description">%description%</div>\n' +
+                    '                            <div class="right clearfix">\n' +
+                    '                                <div class="item__value">%value%</div>\n' +
+                    '                                <div class="item__delete">\n' +
+                    '                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>\n' +
+                    '                                </div>\n' +
+                    '                            </div>\n' +
+                    '                        </div>';
+            } else if (type === 'exp') {
+
+                element = DOMstrings.expensesContainer;
+
+                html = '<div class="item clearfix" id="expense-%id%">\n' +
+                    '                            <div class="item__description">%description%</div>\n' +
+                    '                            <div class="right clearfix">\n' +
+                    '                                <div class="item__value">%value%</div>\n' +
+                    '                                <div class="item__percentage">21%</div>\n' +
+                    '                                <div class="item__delete">\n' +
+                    '                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>\n' +
+                    '                                </div>\n' +
+                    '                            </div>\n' +
+                    '                        </div>';
+            }
+
+            // Replace placeholder text
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function (current, index, array) {
+               current.value = '';
+            });
+
+            fieldsArr[0].focus();
         },
 
         getDOMstrings : function () {
@@ -65,23 +151,28 @@ var controller = (function (budgetCtrl, UICtrl) {
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
         
         document.addEventListener('keypress', function (event) {
-            if(event.keyCode === 13 || event.which === 12) {
+            if(event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
             }
         });
     };
 
     var ctrlAddItem = function () {
-        
+
+        var input, newItem;
+
         // Get input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         console.log(input);
 
         // Add item to the budget controller
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.inputValue);
 
         // Add item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
+        // Clear the input field
+        UICtrl.clearFields();
 
         // Calculate budget
 

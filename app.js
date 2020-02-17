20f32e0f6f2047801275dcc99110a8deaaf056f1
Roman <romansklepovyy1@gmail.com>
2020-02-17 11:33:55 +0200

    DisplayPercentages

--- app.js
+++ app.js
@@ -156,7 +156,8 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expPercentageLabel: '.item__percentage'
     };
 
     return {
@@ -241,6 +242,27 @@ var UIController = (function () {
             }
         },
 
+        displayPercentages: function(percentages) {
+
+            var fields = document.querySelectorAll(DOMstrings.expPercentageLabel);
+
+            var nodeListForEach = function (list, callback) {
+                for (i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function (current, index) {
+
+               if (percentages[index] > 0) {
+                   current.textContent = percentages[index] + '%';
+               } else {
+                   current.textContent = '---';
+               }
+            });
+
+        },
+
         getDOMstrings : function () {
             return DOMstrings;
         }
@@ -288,7 +310,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
 
         // Update the UI
-
+        UICtrl.displayPercentages(percentages);
 
     };
 
